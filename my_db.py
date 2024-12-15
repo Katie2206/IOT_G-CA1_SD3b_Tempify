95fcb11e4a6f8b0b4729e60ac1794e44ae22528d
Katie2206 <katielynch2204@gmail.com>
2024-12-15 17:48:45 +0000

    Display Live Data
    Display live data from PubNub to frontend along with database information.
    Get icons to change depending on the value taken in by the sensors.

--- my_db.py
+++ my_db.py
@@ -59,6 +59,17 @@ def add_plant(Plant_ID, Name, Last_Updated, Current_Temperature, Current_Humidit
         db.session.commit()
 
 
+def update_plant(Plant_ID, Last_Updated, Current_Temperature, Current_Humidity, Current_Soil_Moisture):
+    row = get_plant_if_exists(Plant_ID)
+    if row is not False:
+        updated_plant = Plant(Plant_ID, Last_Updated, Current_Temperature, Current_Humidity, Current_Soil_Moisture)
+        db.session.update(updated_plant)
+        db.session.commit()
+    else:
+        print("That Plant Does Not Exist To Update")
+        return False
+
+
 def view_all_plants():
     rows = Plant.query.all()
     print_plants(rows)
@@ -93,19 +104,39 @@ def delete_revoked_token(Plant_ID):
 
 class Temperature(db.Model):
     __tablename__ = "TemperatureInformation"
+    Temperature_ID = db.Column(db.Integer, primary_key = True)
     Current_Temperature = db.Column(db.Float, nullable=False, primary_key = True)
     Max_Temp = db.Column(db.Float, nullable=False)
     Min_Temp = db.Column(db.Float, nullable=False)
     Ideal_Temp = db.Column(db.Float, nullable=False)
     Error_Margin = db.Column(db.Float, nullable=False)
 
-    def __init__(self, Current_Temperature, Max_Temp, Min_Temp, Ideal_Temp, Error_Margin):
+    def __init__(self, Temperature_ID, Current_Temperature, Max_Temp, Min_Temp, Ideal_Temp, Error_Margin):
+        self.Temperature_ID = Temperature_ID
         self.Current_Temperature = Current_Temperature
         self.Max_Temp = Max_Temp
         self.Min_Temp = Min_Temp
         self.Ideal_Temp = Ideal_Temp
         self.Error_Margin = Error_Margin
     
+
+def get_temp_if_exists(Temperature_ID):
+    get_temp_row = Temperature.query.filter_by(Temperature_ID = Temperature_ID).first()
+    if get_temp_row is not None:
+        return get_temp_row
+    else:
+        print("That Temperature Does Not Exist")
+        return False
+
+
+def update_temp(Temperature_ID, Current_Temperature):
+    row = get_temp_if_exists(Temperature_ID)
+    if row is not False:
+        row.Current_Temperature = Current_Temperature
+        db.session.commit()
+    else:
+        print("That Temperature Does Not Exist To Update")
+        return False
 
 def delete_all_temperatures():
     try:
@@ -127,18 +158,40 @@ def print_temperature(rows):
 
 class Humidity(db.Model):
     __tablename__ = "HumidityInformation"
+    Humidity_ID = db.Column(db.Integer, primary_key = True)
     Current_Humidity = db.Column(db.Float, nullable=False, primary_key = True)
     Max_Humidity = db.Column(db.Float, nullable=False)
     Min_Humidity = db.Column(db.Float, nullable=False)
     Ideal_Humidity = db.Column(db.Float, nullable=False)
     Error_Margin = db.Column(db.Float, nullable=False)
 
-    def __init__(self, Current_Humidity, Max_Humidity, Min_Humidity, Ideal_Humidity, Error_Margin):
+    def __init__(self, Humidity_ID, Current_Humidity, Max_Humidity, Min_Humidity, Ideal_Humidity, Error_Margin):
+        self.Humidity_ID = Humidity_ID
         self.Current_Humidity = Current_Humidity
         self.Max_Humidity = Max_Humidity
         self.Min_Humidity = Min_Humidity
         self.Ideal_Humidity = Ideal_Humidity
         self.Error_Margin = Error_Margin
+
+
+def get_humidity_if_exists(Humidity_ID):
+    get_humidity_row = Humidity.query.filter_by(Humidity_ID = Humidity_ID).first()
+    if get_humidity_row is not None:
+        return get_humidity_row
+    else:
+        print("That Humidity Does Not Exist")
+        return False
+
+
+def update_humidity(Humidity_ID, Current_Humidity):
+    row = get_plant_if_exists(Humidity_ID)
+    if row is not False:
+        updated_humidity = Humidity(Current_Humidity)
+        db.session.update(updated_humidity)
+        db.session.commit()
+    else:
+        print("That Plant Does Not Exist To Update")
+        return False
 
 
 def delete_all_humidity():
@@ -160,12 +213,23 @@ def print_humidity(rows):
 
 class Soil(db.Model):
     __tablename__ = "SoilMoistureInformation"
+    Soil_ID = db.Column(db.Integer, primary_key = True)
     Current_Soil_Moisture = db.Column(db.String(255), nullable=False, primary_key = True)
     Ideal_Moisture_Level = db.Column(db.String(255), nullable=False)
 
-    def __init__(self, Current_Soil_Moisture, Ideal_Moisture_Level):
+    def __init__(self, Soil_ID, Current_Soil_Moisture, Ideal_Moisture_Level):
+        self.Soil_ID = Soil_ID
         self.Current_Soil_Moisture = Current_Soil_Moisture
         self.Ideal_Moisture_Level = Ideal_Moisture_Level
+
+
+def get_soil_if_exists(Soil_ID):
+    get_soil_row = Soil.query.filter_by(Soil_ID = Soil_ID).first()
+    if get_soil_row is not None:
+        return get_soil_row
+    else:
+        print("That Soil Moisture Does Not Exist")
+        return False
 
 
 def delete_all_soil():
@@ -183,3 +247,14 @@ def view_all_soil():
 def print_soil(rows):
     for row in rows:
         print(f"{row.Current_Soil_Moisture} | {row.Ideal_Moisture_Level}")
+
+
+def update_soil(Soil_ID, Current_Soil_Moisture):
+    row = get_soil_if_exists(Soil_ID)
+    if row is not False:
+        updated_soil = Soil(Current_Soil_Moisture)
+        db.session.update(updated_soil)
+        db.session.commit()
+    else:
+        print("That Soil Moisture Does Not Exist To Update")
+        return False
